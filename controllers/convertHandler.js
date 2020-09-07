96e1fb875109c033f405398ad3db558b6c2af82b
Marco <marco.mannucci@hey.com>
2020-09-07 14:36:36 +0200

    refactoring step 1

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -20,28 +20,30 @@ function ConvertHandler() {
   this.getNum = function(input) {
     var result;
     let match = /[a-zA-Z]/.exec(input);
+    
     if (match) {
       result = input.substring(0, match.index);
-    }
-
-    if (!result) {
-      return 1;
-    }
-
-    if (result.indexOf('/') != -1) {
-      let operands = result.split('/');
-
-      if (operands.length != 2) {
-        console.log('throwing error');
-        throw new Error('invalid number');
-      }
-      result = parseFloat(operands[0]) / parseFloat(operands[1]);
-    }
-    else if (result.indexOf('.') != -1) {
-      result = parseFloat(input);
     } else {
-      result = parseFloat(input);
+      throw new Error('invalid input');
     }
+
+    let numerator;
+    let denominator;
+    let operands = result.split('/');
+
+    if (operands.length > 2) {
+      throw new Error('invalid number');
+    }
+
+    if (operands.length >= 1) {
+      numerator = parseFloat(operands[0]);
+    }
+    
+    if (operands.length == 2) {
+      denominator = parseFloat(operands[1]);
+    }
+
+    result = (numerator||1) / (denominator||1)
     
     return result;
   };
